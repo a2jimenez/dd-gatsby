b6f4ae1b5ecb1c5be70872259e5b5f0a074a0333
Adolfo <a2jimenez@gmail.com>
2020-09-03 14:01:40 -0500

    0903-1401

--- src/Hamburger.js
+++ src/Hamburger.js
@@ -1,17 +1,17 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React from 'react';
+import PropTypes from 'prop-types';
 
-import { HamburgerButton } from "./styles/HamburgerStyles"
-import HamburgerHeart from "../images/tango_heart_hamburger.svg"
+import { HamburgerButton } from './styles/HamburgerStyles';
+import HamburgerHeart from '../images/tango_heart_hamburger.svg';
 
-const Hamburger = ({ HandleOverlayMenu }) => (
-  <HamburgerButton onclick={HandleOverlayMenu} tabIndex="0">
+const Hamburger = ({ handleOverlayMenu }) => (
+  <HamburgerButton onClick={handleOverlayMenu} tabIndex="0">
     <img src={HamburgerHeart} alt="tango-hamburger" />
   </HamburgerButton>
-)
+);
 
-Hamburger.PropTypes = {
+Hamburger.propTypes = {
   handleOverlayMenu: PropTypes.func,
-}
+};
 
-export default Hamburger
+export default Hamburger;
--- src/components/OverlayMenu.js
+++ src/components/OverlayMenu.js
@@ -1,5 +1,11 @@
-import React from 'react'
+import React from "react"
+import { Link, useStaticQuery, graphql } from "gatsby"
 
-const OverlayMenu = () => <div>OverlayMenu</div>
+import WhiteLogo from "../images/tango_logo_white.svg"
+import CloseButton from "../images/tango_close_button.svg"
+
+const OverlayMenu = () => {
+  return <div>Overlay Menu</div>
+}
 
 export default OverlayMenu

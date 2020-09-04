7301ca60778caf91896199b906622ca4b5761346
Adolfo <a2jimenez@gmail.com>
2020-09-04 14:45:01 -0500

    0904-1444

---
+++ src/components/BreadCrumb.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const BreadCrumb = () => {
+  return <div>Breadcrumb</div>
+}
+
+export default BreadCrumb
--- src/components/index/About.js
+++ src/components/index/About.js
@@ -1,5 +1,44 @@
-import React from 'react';
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
-const About = () => <div>About</div>;
+import { AboutWrapper } from "./styles/AboutStyles"
 
-export default About;
+const About = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      wordpressPage(wordpress_id: { eq: 47 }) {
+        content
+        featured_media {
+          localFile {
+            childImageSharp {
+              fluid(quality: 100, maxWidth: 2000) {
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <AboutWrapper>
+      <div className="container-fluid">
+        <div className="row">
+          <div
+            className="col-md-6 aboutImage"
+            style={{
+              backgroundImage: `url(${data.wordpressPage.featured_media.localFile.childImageSharp.fluid.src})`,
+            }}
+          />
+          <div className="col-md-6 aboutText">
+            <div
+              dangerouslySetInnerHTML={{ __html: data.wordpressPage.content }}
+            />
+          </div>
+        </div>
+      </div>
+    </AboutWrapper>
+  )
+}
+export default About
